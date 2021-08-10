ef02cfef54d90f525cbcd22f3870aa53c77de068
Ryan Barouki <ryanbarouki@gmail.com>
2021-08-10 22:13:52 +0100

    add web sockets

---
+++ cabo-server/app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const PORT = process.env.PORT || 4001;
+const index = require("./routes/index");
+
+const app = express();
+app.use(index);
+
+const server = http.createServer(app);
+
+const io = socketIo(server, {
+    cors: {
+        origin: '*'
+    }
+}); // < Interesting!
+
+let interval;
+io.on("connection", socket => {
+    console.log("New client connected");
+
+    if (interval) {
+        clearInterval(interval);
+    }
+
+    interval = setInterval(() => getApiAndEmit(socket), 1000);
+    socket.on('disconnect', () => {
+        console.log("Client disconnected");
+        clearInterval(interval);
+    });
+});
+
+const getApiAndEmit = socket => {
+  const response = new Date();
+  // Emitting a new message. Will be consumed by the client
+  socket.emit("FromAPI", response);
+};
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
